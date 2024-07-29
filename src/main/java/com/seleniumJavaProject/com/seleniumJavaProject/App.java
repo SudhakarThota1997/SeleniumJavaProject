ffe287fa4a9ebb7e1dc460dcb759e376f287b6f6
thota <thota@DESKTOP-HSJJE2V>
2024-07-29 11:44:14 +0530

    first commit

---
+++ src/main/java/com/seleniumJavaProject/com/seleniumJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.seleniumJavaProject.com.seleniumJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/day1/DatePicker2.java
@@ -0,0 +1,13 @@
+package day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DatePicker2 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+	}
+
+}
